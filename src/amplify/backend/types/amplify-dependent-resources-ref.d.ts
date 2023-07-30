34299b275353d30ca7647c95620efa6f819dea94
Andrew Swan <andrewswan@Andrews-MBP-4.lan>
2023-07-30 10:39:20 -0400

    added api and a dynamodb database

--- src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphql23b3cb744": {
       "AppClientID": "string",
